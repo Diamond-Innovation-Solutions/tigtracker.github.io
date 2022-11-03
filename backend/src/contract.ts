139682ca0738dcb80299f18b9efe026b5b3eb1e7
SNIAPA <wasilewskihubers@gmail.com>
2022-11-03 11:26:35 +0100

    working on the dataModels

--- backend/src/contract.ts
+++ backend/src/contract.ts
@@ -1,7 +1,16 @@
 import { Contract as EthContract } from "web3-eth-contract"
 
+type address = string
+
 export default interface Contract extends EthContract {
   events:{
-    AssetAdded: (callback:(internalType:number)=>void)=>void
+    AssetAdded: (callback:(error:Error|null,events:any)=>void)=>void
+    OwnershipTransfered: (callback:(error:Error|null,events:any)=>void)=>void 
+    ReferralCreated: (callback:(error:Error|null,events:any)=>void)=>void 
+    Referred: (callback:(error:Error|null,events:any)=>void)=>void 
+  }
+  methods:{
+    addAsset:(asset:number,name:string,_chainlinkFed:address,minLeverage:number,maxLeverage:number,feeMultiplier:number,baseFundingRate:number)=>void
+
   }
 }
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,24 +3,34 @@ import { AbiItem, } from 'web3-utils'
 import Contract from './contract'
 
 
-;(async () => {
-  const abi = JSON.parse(
-    (
-      await (
-        await fetch(
-          'https://api.arbiscan.io/api?module=contract&action=getabi&address=0x0e6E91221C46904563fAfDCc814fbF342BE8Ba20'
-        )
-      ).json()
-    ).result
-  ) as AbiItem[]
+  ; (async () => {
+    const abi = JSON.parse(
+      (
+        await (
+          await fetch(
+            'https://api.arbiscan.io/api?module=contract&action=getabi&address=0x0e6E91221C46904563fAfDCc814fbF342BE8Ba20'
+          )
+        ).json()
+      ).result
+    ) as AbiItem[]
 
-  const web3 = new Web3(new Web3.providers.HttpProvider('https://rpc.flashbots.net/'))
+    const web3 = new Web3('wss://rinkeby-light.eth.linkpool.io/ws')
 
 
-  const contract = new web3.eth.Contract(
-    abi,
-    '0x0e6E91221C46904563fAfDCc814fbF342BE8Ba20'
-  ) as Contract
-  contract.events.AssetAdded((...args:any)=>console.log(JSON.stringify(args)))
+    console.log(JSON.stringify(abi,null,4))
 
-})()
+    const contract = new web3.eth.Contract(
+      abi,
+      '0x0e6E91221C46904563fAfDCc814fbF342BE8Ba20'
+    ) as Contract
+
+    
+
+    contract.events.AssetAdded((error, event)=>{
+      if(error)
+        console.log(`error: ${error}`)
+        return
+      console.log(`event: ${event}`)
+    })
+
+  })()
